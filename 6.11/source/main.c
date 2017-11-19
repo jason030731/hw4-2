76bcdda540a2123a25b5f009a480050f31485245
liu <jason030731@gmail.com>
2017-11-20 03:17:08 +0800

    HW4-2

---
+++ 6.11/source/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 10
+int main(void)
+{
+	int a[SIZE] = {2,6,4,8,10,12,89,68,45,37};
+	int b=0,c=1,d=1;
+	int pass;
+	size_t i;
+	int hold;
+	puts("Data items in original order");
+
+	for (i = 0; i < SIZE;++i) 
+	{
+		printf("%4d",a[i]);
+	}
+
+	for (pass = 1; pass < SIZE; ++pass)
+	{
+		
+		do {
+			for (i = 0; i < SIZE - d; ++i)
+			{
+				if (a[i] > a[i + 1])
+				{
+					hold = a[i];
+					a[i] = a[i + 1];
+					a[i + 1] = hold;
+					c++;
+				}
+				b++;
+			}
+		} while (c > b);
+		d++;
+	}
+	puts("\nData items in ascending order");
+	for (i = 0; i < SIZE;++i) 
+	{
+		printf("%4d",a[i]);
+	}
+	printf("\n");
+	system("pause");
+	return 0;
+}
